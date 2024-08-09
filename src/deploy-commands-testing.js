698c30a7905c096ea52064e8b676e7bbe10a18d1
perttunurmi <perttu.nurmi+github@gmail.com>
2024-08-09 06:16:52 +0000

    better command deployment

--- src/deploy-commands-testing.js
+++ src/deploy-commands-testing.js
@@ -29,23 +29,18 @@ for (const folder of commandFolders) {
 
 // Construct and prepare an instance of the REST module
 const rest = new REST().setToken(token);
-
-for (let i in guildIds) {
-	// and deploy your commands!
-	(async () => {
-		try {
-
-			console.log(`Started refreshing ${commands.length} application (/) commands.`);
-			// The put method is used to fully refresh all commands in the guild with the current set
-			const data = await rest.put(
-				Routes.applicationGuildCommands(clientId, guildIds[i]),
-				{ body: commands },
-			);
-
-			console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-		} catch (error) {
-			// And of course, make sure you catch and log any errors!
-			console.error(error);
-		}
-	})()
-};
+// and deploy your commands!
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+})()
