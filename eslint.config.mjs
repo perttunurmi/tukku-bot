ee33b93399485cd12335855393131eaf3ce51d6a
perttunurmi <perttu.nurmi@gmail.com>
2025-06-16 06:42:29 +0000

    eslint rules

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import json from "@eslint/json";
+import { defineConfig } from "eslint/config";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+  {
+    plugins: {},
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: { ...globals.node },
+      ecmaVersion: "latest",
+    },
+    rules: {
+      "prefer-const": "warn",
+      eqeqeq: "error",
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'no-console': 'off',
+    },
+  },
+]);
